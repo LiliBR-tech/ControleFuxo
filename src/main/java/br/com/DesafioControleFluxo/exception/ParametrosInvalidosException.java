3a86dba45ab1e8f05cbf5b093d72057341338305
Lili <lilian.bootcamps@gmail.com>
2025-02-14 16:31:28 -0300

    ⚠️ Tratamento de exceção personalizada

---
+++ src/main/java/br/com/DesafioControleFluxo/exception/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package br.com.DesafioControleFluxo.exception;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
